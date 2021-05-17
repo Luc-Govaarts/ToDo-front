6bd499a4e90fe5cc82beff1e45b4a3b0a17b720f
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-17 16:24:04 +0200

    login form setup with material UI, not funcionral

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,21 +1,102 @@
 import React, { useState } from 'react'
+import {
+	Button,
+	Container,
+	Avatar,
+	TextField,
+	Link,
+	Typography,
+	Box,
+} from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}))
 
 export default function Login() {
-	const [username, setUsername] = useState('')
+	const classes = useStyles()
+	const [email, set_email] = useState('')
 	const [password, set_password] = useState('')
 
+	function submitForm(event) {
+		event.preventDefault()
+
+		// dispatch(login(email, password))
+
+		set_email('')
+		set_password('')
+	}
+
 	return (
-		<div>
-			<div>
-				<p>this is the login Page!!</p>
-			</div>
-            <div>
-                <form>
-                    <p>
-                        Login form
-                    </p>
-                </form>
-            </div>
-		</div>
+		<Box mt={12}>
+			<Container component='main' maxWidth='xs'>
+				<div className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Log in
+					</Typography>
+					<form className={classes.form} noValidate>
+						<TextField
+							onChange={(event) => set_email(event.target.value)}
+							value={email}
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email'
+							name='email'
+							autoComplete='email'
+							autoFocus
+						/>
+						<TextField
+							value={password}
+							onChange={(event) => set_password(event.target.value)}
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Wachtwoord'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+						/>
+						<Button
+							onClick={submitForm}
+							type='submit'
+							fullWidth
+							variant='contained'
+							color='primary'
+							className={classes.submit}>
+							Log in
+						</Button>
+						<Link href='/signup' variant='body2'>
+							{'Nog geen account? Schrijf je in'}
+						</Link>
+					</form>
+				</div>
+			</Container>
+		</Box>
 	)
 }
