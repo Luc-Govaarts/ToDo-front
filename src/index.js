7d95b2937d23212ad0d6f3f15decbce4d1579b20
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-17 16:14:20 +0200

    setting up Material UI Theme

--- src/index.js
+++ src/index.js
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { lightGreen, lightBlue } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+	spacing: 8,
+	palette: {
+		primary: {
+			main: lightGreen[400],
+		},
+		secondary: {
+			main: lightBlue[400],
+		},
+	},
+})
 
 ReactDOM.render(
- <React.StrictMode>
-   <Router>
-     <App />
-   </Router>
- </React.StrictMode>,
- document.getElementById("root")
-);
+	<React.StrictMode>
+		<Router>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
